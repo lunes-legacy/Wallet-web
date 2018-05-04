1fc5167ac839118b27693e9bc04ecc5136a1cdea
Leonardo Ribeiro <leo.ribeiro@lunes.io>
2018-05-03 22:51:01 -0300

    Final of slide image

--- src/shared/containers/User/Login/Slide.js
+++ src/shared/containers/User/Login/Slide.js
@@ -1,57 +1,40 @@
 import React from "react";
+import Image from "../../../components/Image";
 import styled from "styled-components";
 
 let Container = styled.div`
   margin: 0 auto;
-  margin-top: 20px;
-  position: relative;
-  width: $big_width;
-  height: 0;
-  padding-bottom: $big_height;
-  user-select: none;
-  background-color: #1c1c1c;
-  box-shadow: $box-shadow;
+  position: center;
+  margin-top: 50px;
+  margin-left: 20%;
+  margin-right: 11%;
+  width: 80%;
 `;
 
-let SlideImage = styled.div`
-  position: absolute;
-  width: 50%;
-  height: 50%;
-  z-index: -1;
-`;
-
-let SlideNav = styled.nav`
-  width: 100%;
-  bottom: 12%;
-  height: 11px;
-  position: absolute;
-  text-align: center;
-  z-index: 99;
-  cursor: default;
-`;
+let images = [
+  {
+    link:
+      "https://cdn.discordapp.com/attachments/441307920714694656/441312361442181140/Browser_window1.png",
+    text: `Aproveite da usabilidade e tecnologia desta incrível carteira e armazene suas criptomoedas com um alto nível de segurança.`
+  },
+  {
+    link:
+      "https://cdn.discordapp.com/attachments/441307920714694656/441312361794502657/Browser_window2.png",
+    text: "EXEMPLO2"
+  },
+  {
+    link:
+      "https://cdn.discordapp.com/attachments/441307920714694656/441312363249926154/Browser_window3.png",
+    text: "EXEMPLO3"
+  }
+];
 
 class Slide extends React.Component {
   render() {
     return (
       <section>
         <Container>
-          <SlideImage>
-            <img src="http://www.bhmpics.com/wallpapers/little_pony_art-800x480.jpg" />
-          </SlideImage>
-
-          <SlideNav>
-            <label htmlFor={"i1"} className="dots" id="dot1" />
-            <label htmlFor={"i2"} className="dots" id="dot2" />
-            <label htmlFor={"i3"} className="dots" id="dot3" />
-            <label htmlFor={"i4"} className="dots" id="dot4" />
-          </SlideNav>
-
-          <div>
-            <input type={"radio"} id="i1" name="images" />
-            <input type={"radio"} id="i2" name="images" />
-            <input type={"radio"} id="i3" name="images" />
-            <input type={"radio"} id="i4" name="images" />
-          </div>
+          <Image imageList={images} />
         </Container>
       </section>
     );
