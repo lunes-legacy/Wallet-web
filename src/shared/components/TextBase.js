c72f0575c7f2b5e366929e5886781574cdb294ca
Leonardo Ribeiro <leo.ribeiro@lunes.io>
2018-05-07 16:37:05 -0300

    Adjusting font of components. first part

--- src/shared/components/TextBase.js
+++ src/shared/components/TextBase.js
@@ -1,60 +1,65 @@
-import { css } from 'styled-components';
-import style from 'Shared/style-variables';
+import { css } from "styled-components";
+import style from "Shared/style-variables";
 
 export let TextBase = css`
 	display: block;
 	${props => {
-		if (props.fontSize) {
-			return `font-size: ${props.fontSize};`;
-		}
+    if (props.fontSize) {
+      return `font-size: ${props.fontSize};`;
+    }
 
-		return 'font-size: 1.8rem;';
-	}}
+    return "font-size: 1.8rem;";
+  }}
 	${props => {
-		if (props.clNormalGreen) {
-			return `color: ${style.normalGreen}`;
-		} else if(props.clWhite) {
-			return `color: white`;
-		} else if (props.clNormalLilac) {
-			return `color: ${style.normalLilac}`;
-		}
-	}}
+    if (props.clNormalGreen) {
+      return `color: ${style.normalGreen}`;
+    } else if (props.clWhite) {
+      return `color: white`;
+    } else if (props.clNormalLilac) {
+      return `color: ${style.normalLilac}`;
+    }
+  }}
 	${props => {
-		if (props.txCenter) {
-			return 'text-align: center';
-		} else if (props.txRight) {
-			return 'text-align: right';
-		}
-	}}
+    if (props.txCenter) {
+      return "text-align: center";
+    } else if (props.txRight) {
+      return "text-align: right";
+    }
+  }}
 	${props => {
-		if (props.margin) {
-			return 'margin: ' + props.margin + ';';
-		}
-	}}
+    if (props.margin) {
+      return "margin: " + props.margin + ";";
+    }
+  }}
 	${props => {
-		if (props.padding) {
-			return 'padding: ' + props.padding + ';';
-		}
-	}}
+    if (props.padding) {
+      return "padding: " + props.padding + ";";
+    }
+  }}
 	${props => {
-		if (props.size && props.size.indexOf('rem') !== -1) {
-			return `font-size: ${props.size}`;
-		}
-	}}
+    if (props.size && props.size.indexOf("rem") !== -1) {
+      return `font-size: ${props.size}`;
+    }
+  }}
 	${props => {
-		if (props.txBold) {
-			return 'font-weight: bold;';
-		} else if (props.txLight) {
-			return 'font-weight: 100;';
-		} else if (props.txNormal) {
-			return 'font-weight: 300;';
-		}
-	}}
+    if (props.txBold) {
+      return "font-weight: bold;";
+    } else if (props.txLight) {
+      return "font-weight: 100;";
+    } else if (props.txNormal) {
+      return "font-weight: 300;";
+    }
+  }}
 	${props => {
-		if (props.txInline) {
-			return 'display: inline;';
-		} else if (props.txInlineBlock){
-			return 'display: inline-block;';
-		}
-	}}
+    if (props.txInline) {
+      return "display: inline;";
+    } else if (props.txInlineBlock) {
+      return "display: inline-block;";
+    }
+  }}
+  ${props => {
+    if (props.offSide) {
+      return "font-family: OffSide";
+    }
+  }}
 `;
--- src/shared/containers/AppSwitcher.js
+++ src/shared/containers/AppSwitcher.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history'; 
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
-import App      from 'Containers/App/index';
-import Login    from 'Containers/User/Login/index';
-import Registry from 'Containers/User/Registry/index';
-import Reset    from 'Containers/User/Reset/index';
-
+import App from "Containers/App/index";
+import Login from "Containers/User/Login/index";
+import Registry from "Containers/User/Registry/index";
+import Reset from "Containers/User/Reset/index";
 
 class AppSwitcher extends React.Component {
-	render()
-	{
-		return (
-			<Switch>
-				<Route strict path={'/app'} component={App}/>
-				<Route exact path={'/login'} component={Login}/>
-				<Route exact path={'/registry'} component={Registry}/>
-				<Route exact path={'/reset'} component={Reset}/>
-			</Switch>
-		);
-	}
+  render() {
+    return (
+      <Switch>
+        <Route strict path={"/app"} component={App} />
+        <Route exact path={"/login"} component={Login} />
+        <Route exact path={"/registry"} component={Registry} />
+        <Route exact path={"/reset"} component={Reset} />
+      </Switch>
+    );
+  }
 }
 
 export default AppSwitcher;
