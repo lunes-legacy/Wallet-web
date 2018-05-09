65f20969b088bda6a023c57d66e45e26ad5a752e
Marcelo <marcelorafaeldebrito@gmail.com>
2018-05-09 18:49:38 -0300

    forms/select 90%, falta colocar o tema personalizável

--- src/shared/components/forms/select/Opt.js
+++ src/shared/components/forms/select/Opt.js
@@ -1,17 +1,20 @@
 import styled from 'styled-components';
+import style  from 'Shared/style-variables';
+
 import { TextBase } from 'Components/TextBase';
 import { bounce_up_select_option } from './keyframes';
 
 let Opt = styled.div`
 	${TextBase};
 	width: 100%;
-	background: white;
+	background: ${style.lightLilac};
 	transform: scale(1);
 	animation-origin: top;
 	animation-duration: 0.1s;
 	cursor: pointer;
 	padding: 5px;
 	user-select: none;
+	color: white;
 	&:last-child {
 		border-bottom-left-radius: 0.5rem;
 		border-bottom-right-radius: 0.5rem;
