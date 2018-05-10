4f9067a69a392ba4e0ab6c147f67f8e1b5300108
Leonardo Ribeiro <leo.ribeiro@lunes.io>
2018-05-10 19:17:38 -0300

    Início de desenvolvimento do gráfico

--- src/shared/classes/Wallet.js
+++ src/shared/classes/Wallet.js
@@ -1,48 +1,48 @@
-import { errorPattern } from 'Utils/functions';
-import { coins } from 'lunes-lib';
-import sb from 'satoshi-bitcoin';
+import { errorPattern } from "Utils/functions";
+import { coins } from "lunes-lib";
+import sb from "satoshi-bitcoin";
 
 export class WalletClass {
-	static coinsPrice;
+  static coinsPrice;
 
-	getCoinsPrice = async (data) => {
-		// EX: data = {
-		// 	{fromSymbol, toSymbol, exchange= 'CCCAGG'}
-		// }
-		try {
-			let coinsPrice = {};
-			for (let coinKey in data) {
-				coinsPrice[data[coinKey].fromSymbol.toLowerCase()] = await coins.getPrice(data[coinKey]);
-			}
-			return coinsPrice;
-		} catch(err) {
-			return errorPattern(`Error on trying to get price`, 500, 'COINGETPRICE_ERROR', err);
-		}
-	}
-	/*
+  getCoinsPrice = async data => {
+    // EX: data = {
+    // 	{fromSymbol, toSymbol, exchange= 'CCCAGG'}
+    // }
+    try {
+      let coinsPrice = {};
+      for (let coinKey in data) {
+        coinsPrice[data[coinKey].fromSymbol.toLowerCase()] = await coins.getPrice(data[coinKey]);
+      }
+      return coinsPrice;
+    } catch (err) {
+      return errorPattern(`Error on trying to get price`, 500, "COINGETPRICE_ERROR", err);
+    }
+  };
+  /*
 		@param user: typically it comes from cookies
 		returns: {btc: ['address','address', ...]}
 	*/
-	getUserAddresses = (user) => {
-		try {
-			let addresses = {};
-			//(example): @param coin = {symbol: 'btc', createdAt: [timestamp], etc..} 
-			user.wallet.coins.map((coin) => {
-				//if addresses does not have {addresses['btc'] (example)} as attribute, so:
-				if (!addresses[coin.symbol]) {
-					addresses[coin.symbol] = [];
-				}
-				//we get the ${addresses[coin.symbol]} array, and we push an address to it
-				coin.addresses.map((obj) => {
-					addresses[coin.symbol].push(obj.address);
-				});
-			});
-			return addresses;
-		} catch (err) {
-			return errorPattern('Was not possible get user addresses',500,'WALLET_GETUSERADDRESS_ERROR', err);
-		}
-	}
-	/*
+  getUserAddresses = user => {
+    try {
+      let addresses = {};
+      //(example): @param coin = {symbol: 'btc', createdAt: [timestamp], etc..}
+      user.wallet.coins.map(coin => {
+        //if addresses does not have {addresses['btc'] (example)} as attribute, so:
+        if (!addresses[coin.symbol]) {
+          addresses[coin.symbol] = [];
+        }
+        //we get the ${addresses[coin.symbol]} array, and we push an address to it
+        coin.addresses.map(obj => {
+          addresses[coin.symbol].push(obj.address);
+        });
+      });
+      return addresses;
+    } catch (err) {
+      return errorPattern("Was not possible get user addresses", 500, "WALLET_GETUSERADDRESS_ERROR", err);
+    }
+  };
+  /*
 		@param user: typically comes from cookies
 		return ex:
 			{ 
@@ -53,57 +53,64 @@ export class WalletClass {
 				} 
 			}
 	*/
-	getBalance = async (user) => {
-		if (typeof user === 'string') {
-			user = JSON.parse(user);
-		}
-		try {
-			let coinsPrice = await this.getCoinsPrice([
-				{fromSymbol: 'BTC', toSymbol: 'BRL,USD'},
-				{fromSymbol: 'LTC', toSymbol: 'BRL,USD'},
-				{fromSymbol: 'ETH', toSymbol: 'BRL,USD'},
-			]);
-			this.coinsPrice = coinsPrice;
-			let addresses   = this.getUserAddresses(user);
-			let balance     = {};
-			let token       = user.accessToken;
-			//coin = 'btc' (example)
-			for (let coin in addresses) {
-				//addressKey = 1 (example)
-				let i = 0;
-				for (let addressKey in addresses[coin]) {
-					//it gets the current addres of the iteration
-					let address        = addresses[coin][addressKey];
-					//it returns a response object
-					let response       = await coins.bitcoin.getBalance({ address }, token);
-					if (response.status === 'success') {
-						if (!balance[coin]) {
-							balance[coin] = {};
-							balance[coin]['total_confirmed']   = sb.toSatoshi(0);
-							balance[coin]['total_unconfirmed'] = sb.toSatoshi(0);
-							balance[coin]['total_amount']            = 0;
-						}
-						//it sums the old total_confirmed with the new
-						balance[coin]['total_confirmed']   += sb.toSatoshi(response.data.confirmed_balance);
-						balance[coin]['total_unconfirmed'] += sb.toSatoshi(response.data.unconfirmed_balance);
-						//it converts total_confirmed to bitcoin
-						balance[coin]['total_unconfirmed'] = sb.toBitcoin(balance[coin]['total_unconfirmed']);
-						balance[coin]['total_confirmed']   = sb.toBitcoin(balance[coin]['total_confirmed']);
-						balance[coin]['total_amount']      = balance[coin]['total_confirmed'] * coinsPrice[coin]['USD'];
-					}
-				}
-			}
-			return balance;
-		} catch(err) {
-			throw errorPattern('Error on get balance',500,'WALLET_GETBALANCE_ERROR', err);
-		}
-	}
+  getBalance = async user => {
+    if (typeof user === "string") {
+      user = JSON.parse(user);
+    }
+    try {
+      let coinsPrice = await this.getCoinsPrice([
+        { fromSymbol: "BTC", toSymbol: "BRL,USD" },
+        { fromSymbol: "LTC", toSymbol: "BRL,USD" },
+        { fromSymbol: "ETH", toSymbol: "BRL,USD" }
+      ]);
+      this.coinsPrice = coinsPrice;
+      let addresses = this.getUserAddresses(user);
+      let balance = {};
+      let token = user.accessToken;
+      //coin = 'btc' (example)
+      for (let coin in addresses) {
+        //addressKey = 1 (example)
+        let i = 0;
+        for (let addressKey in addresses[coin]) {
+          //it gets the current addres of the iteration
+          let address = addresses[coin][addressKey];
+          //it returns a response object
+          let response = await coins.bitcoin.getBalance({ address }, token);
+          if (response.status === "success") {
+            if (!balance[coin]) {
+              balance[coin] = {};
+              balance[coin]["total_confirmed"] = sb.toSatoshi(0);
+              balance[coin]["total_unconfirmed"] = sb.toSatoshi(0);
+              balance[coin]["total_amount"] = 0;
+            }
+            //it sums the old total_confirmed with the new
+            balance[coin]["total_confirmed"] += sb.toSatoshi(response.data.confirmed_balance);
+            balance[coin]["total_unconfirmed"] += sb.toSatoshi(response.data.unconfirmed_balance);
+            //it converts total_confirmed to bitcoin
+            balance[coin]["total_unconfirmed"] = sb.toBitcoin(balance[coin]["total_unconfirmed"]);
+            balance[coin]["total_confirmed"] = sb.toBitcoin(balance[coin]["total_confirmed"]);
+            balance[coin]["total_amount"] = balance[coin]["total_confirmed"] * coinsPrice[coin]["USD"];
+          }
+        }
+      }
+      return balance;
+    } catch (err) {
+      throw errorPattern("Error on get balance", 500, "WALLET_GETBALANCE_ERROR", err);
+    }
+  };
 
-	getHistory = async ({address='1Q7Jmho4FixWBiTVcZ5aKXv4rTMMp6CjiD', accessToken='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhZGYzNTZlZjI2Mjk0MGRhMDY0N2M0NSIsInBob25lVmVyaWZpZWQiOm51bGwsInBpbiI6bnVsbCwidHdvZmFFbmFibGVkIjpudWxsLCJpYXQiOjE1MjQ3NTAwNTEsImV4cCI6MTUyNDc1NzI1MX0.ONXUF-aaaO17xCf1L3EXwzZ1oWZ_2EMdQw-0uPvJyHo'}) => {
-		try {
-			return coins.bitcoin.getHistory({address}, accessToken);
-		} catch (err) {
-			return errorPattern('Error on get history', 500, 'WALLET_GETHISTORY_ERROR', err);
-		}
-	}
-};
+  getHistory = async ({
+    address = "1Q7Jmho4FixWBiTVcZ5aKXv4rTMMp6CjiD",
+    accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhZGYzNTZlZjI2Mjk0MGRhMDY0N2M0NSIsInBob25lVmVyaWZpZWQiOm51bGwsInBpbiI6bnVsbCwidHdvZmFFbmFibGVkIjpudWxsLCJpYXQiOjE1MjQ3NTAwNTEsImV4cCI6MTUyNDc1NzI1MX0.ONXUF-aaaO17xCf1L3EXwzZ1oWZ_2EMdQw-0uPvJyHo"
+  }) => {
+    try {
+      return coins.bitcoin.getHistory({ address }, accessToken);
+    } catch (err) {
+      return errorPattern("Error on get history", 500, "WALLET_GETHISTORY_ERROR", err);
+    }
+  };
+
+  getTransactionHistory = async (object) => {
+    return await coins.getHistory(object);
+  };
+}
--- src/shared/containers/Wallet/PanelRight/CoinGraph.js
+++ src/shared/containers/Wallet/PanelRight/CoinGraph.js
@@ -1,34 +1,44 @@
-import React from 'react';
-import styled from 'styled-components';
-import style from 'Shared/style-variables';
-import { LineChart, Line, XAxis, Tooltip } from 'recharts';
-
+import React from "react";
+import styled from "styled-components";
+import style from "Shared/style-variables";
+import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
+import { WalletClass } from "Classes/Wallet";
 class CoinGraph extends React.Component {
-  render() {
-    // APENAS PARA TESTE
-    const data = [
-      {date: '28/04', price: 3830.99},
-      {date: '29/04', price: 4088.22},
-      {date: '30/04', price: 4029.93},
-      {date: '01/05', price: 3980.01},
-      {date: '02/05', price: 4340.88},
-      {date: '03/05', price: 4320.29},
-      {date: '04/05', price: 4450.92}
-    ];
+  constructor() {
+    super();
 
+    this.state = {
+      history_time_price: []
+    };
+  }
+
+  componentWillMount() {
+    this.coinHistory();
+  }
+
+  coinHistory = async () => {
+    let wallet = new WalletClass();
+    let obj = { fromSymbol: "BTC", toSymbol: "USD", range: "RANGE_1M" };
+    let dataHistory = await wallet.getTransactionHistory(obj);
+
+    this.setState(() => {
+      return {
+        history_time_price: dataHistory.data
+      };
+    });
+  };
+
+  render() {
     return (
       <div>
-        <LineChart
-          width={250}
-          height={175}
-          data={data}
-          >
-          <XAxis dataKey="date"/>
-          <Tooltip/>
-          <Line dataKey="price" stroke="#4cd566"/>
+        <LineChart width={500} height={175} data={this.state.history_time_price}>
+          <XAxis hide dataKey="time" />
+          <YAxis hide domain={["dataMin", "dataMax"]} />
+          <Tooltip />
+          <Line dataKey="close" stroke="#4cd566" />
         </LineChart>
       </div>
-    )
+    );
   }
 }
 
