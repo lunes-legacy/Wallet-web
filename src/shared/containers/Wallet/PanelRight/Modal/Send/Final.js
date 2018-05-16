52b392356ddb7cf9b211a7fb2984b557742b4056
Marcelo <marcelorafaeldebrito@gmail.com>
2018-05-16 18:56:58 -0300

    Mesmo commit de antes, deu erro e nao comitou alguns arquivos

---
+++ src/shared/containers/Wallet/PanelRight/Modal/Send/Final.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { H1 } from 'Components';
+
+class Final extends React.Component {
+	render() {
+		return(
+			<H1>Final</H1>
+		);
+	}
+}
+
+export default Final;
---
+++ src/shared/containers/Wallet/PanelRight/Modal/Send/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//COMPONENTS
+import { H1 } from 'Components';
+
+class Loading extends React.Component {
+	render() {
+		return(
+			<H1>CARREGANDO</H1>
+		);
+	}
+}
+
+export default Loading;
