52960ce5bd35499ba452114994850aee16210c69
Hellen FS <hellen.ferreira@lunes.io>
2018-05-26 15:12:49 -0300

    margin-top and font size

--- src/shared/containers/User/Reset/index.js
+++ src/shared/containers/User/Reset/index.js
@@ -21,6 +21,10 @@ const CustomLogo = Logo.extend`
   margin: 70px auto 0 auto;
 `;
 
+const MarginH1 = styled.div`
+  margin-top:30%;
+`;
+
 const CustomLinkRight = CustomLink.extend`
   text-align: right;
 `;
@@ -74,7 +78,7 @@ class Reset extends React.Component {
     let emailEl = document.querySelector(".reset-email");
     this.props.userReset(emailEl.value);
   };
-  componentDidMount() {}
+  componentDidMount() { }
   componentDidUpdate() {
     this.handleStatus();
   }
@@ -105,13 +109,15 @@ class Reset extends React.Component {
       <div>
         <PanelLeft className={"js-first-panel-left"}>
           <CustomLogo />
-
-          <H1 clNormalGreen txCenter margin={"100px auto 0 auto"}>
-            Esqueceu sua senha?
-          </H1>
-
+          <MarginH1>
+            <H1 clNormalGreen txCenter margin={"100px auto 0 auto"} fontSize={"2.5rem"}>
+              <strong>
+                Esqueceu sua senha?
+              </strong>
+            </H1>
+          </MarginH1>
           <P clWhite txCenter margin={"20px"} fontSize={"1.4rem"}>
-            Nós enviaremos suas instruções de como redefini-la.
+            Nós enviaremos suas instruções sobre como redefini-la.
           </P>
 
           <CustomForm onSubmit={this.handleSubmit}>
