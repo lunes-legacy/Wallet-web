11953c7dc4ba5e4539b4e9ee8dffd3372cdde039
Marcelo <marcelorafaeldebrito@gmail.com>
2018-05-28 15:52:51 -0600

    test-component

--- tests/components/index.js
+++ tests/components/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import toRender from './dev/gabriel';
-// import toRender from './dev/marcelo';
+// import toRender from './dev/gabriel';
+import toRender from './dev/marcelo';
 
 render(toRender(), document.querySelector(".root"));
