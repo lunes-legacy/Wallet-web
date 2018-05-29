3c25c61be3c3b28737cc66f41dc1f8c0d94d99e3
Thiago Pereira <thiago_salper@hotmail.com>
2018-05-29 11:00:48 -0300

    add others cois to sidebar wallet

--- src/shared/redux/reducers/balanceReducer.js
+++ src/shared/redux/reducers/balanceReducer.js
@@ -2,17 +2,38 @@ let initialState = {
 	BTC: { 
 		total_confirmed: 2.9, 
 		total_unconfirmed: 1,
-		total_amount: 3.9
+		total_amount: 3.9, 
+		img: 'btc.svg'
 	},
 	ETH: { 
 		total_confirmed: 1.2, 
 		total_unconfirmed: 1,
-		total_amount: 2.2
+		total_amount: 2.2, 
+		img: 'eth.svg'
 	},
 	LNS: {
 		total_confirmed: 100, 
 		total_unconfirmed: 0,
-		total_amount: 100
+		total_amount: 100, 
+		img: 'lns.svg'
+	},
+	LTC: {
+		total_confirmed: 100, 
+		total_unconfirmed: 0,
+		total_amount: 100, 
+		img: 'ltc.svg'
+	},
+	DASH: {
+		total_confirmed: 100, 
+		total_unconfirmed: 0,
+		total_amount: 100, 
+		img: 'dash.svg'
+	},
+	NANO: {
+		total_confirmed: 100, 
+		total_unconfirmed: 0,
+		total_amount: 100, 
+		img: 'nano.svg'
 	}
 }
 const balanceReducer = (state = initialState, action) => {
