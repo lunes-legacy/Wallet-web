ad1e8a35a14030e61f98e4e8d02932a24784007a
Leonardo Ribeiro <leo.ribeiro@lunes.io>
2018-05-29 11:50:43 -0300

    adjustment on the modal receive

--- src/shared/containers/Wallet/PanelRight/Modal/Background.js
+++ src/shared/containers/Wallet/PanelRight/Modal/Background.js
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 import style from 'Shared/style-variables';
 
 let Background = styled.div`
+	position: fixed;
+	top: 10px;
+	left: 0px;
 	width: 100%;
 	height: 100%;
-	position: fixed;
 	display: flex;
 	justify-content: center;
 	align-items: flex-end;
