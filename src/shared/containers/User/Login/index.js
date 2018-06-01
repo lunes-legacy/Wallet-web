15338fe86c1b49cc4bde209190235c98029e111c
Hellen FS <hellen.ferreira@lunes.io>
2018-06-01 15:55:36 -0300

    full login validation

--- src/shared/containers/User/Login/index.js
+++ src/shared/containers/User/Login/index.js
@@ -60,22 +60,18 @@ class Login extends React.Component {
     if (!validator.isEmail(emailEl.value)) {
       errors.push('Um email válido deve ser informado');
     }
-
     const passRules = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!_+=@#-$%^&*])(?=.{8,})/g;
     if (!validator.matches(passEl.value, passRules)) {
       errors.push('Senha inválida');
     }
-
-
     if (errors.length > 0) {
       alert('- ' + errors.join('\n- '));
       return;
     }
-
   };
 
   handleStatus() {
-    try {      
+    try {
       let statusEl = document.querySelector(".js-status");
       let { status } = this.props.user;
 
@@ -87,64 +83,64 @@ class Login extends React.Component {
         statusEl.textContent = "Tente novamente";
       }
     }
-   catch(err) {
-    console.warn("There's an error on handleStatus", 500, 'HANDLE_STATUS_ERROR', err);
+    catch (err) {
+      console.warn("There's an error on handleStatus", 500, 'HANDLE_STATUS_ERROR', err);
+    }
+
   }
-  
-}
 
-componentDidUpdate() {
-  setTimeout(() => {
-    this.handleStatus();
-  },300);
-}
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.handleStatus();
+    }, 300);
+  }
 
 
-render() {
-  let { status, logged } = this.props.user;
-  return (
-    <div>
-      <PanelLeft>
-        <CustomLogo />
+  render() {
+    let { status, logged } = this.props.user;
+    return (
+      <div>
+        <PanelLeft>
+          <CustomLogo />
 
-        <WrapPhrases>
-          <H1 clNormalGreen txCenter>
-            Rápida, segura e inteligente!
+          <WrapPhrases>
+            <H1 clNormalGreen txCenter>
+              Rápida, segura e inteligente!
             </H1>
-          <P clWhite txCenter margin={"20px 0 70px 0"} fontSize={"1.4rem"}>
-            Entre com seus dados
+            <P clWhite txCenter margin={"20px 0 70px 0"} fontSize={"1.4rem"}>
+              Entre com seus dados
             </P>
-        </WrapPhrases>
+          </WrapPhrases>
 
-        <Form margin={"80px auto"} width={"80%"}>
-          <FormGroup>
-            <Input placeholder={"nome@email.com"} className={"login-email"} placeholder={"E-mail"} type={"email"} required/>
-          </FormGroup>
-          <FormGroup>
-            <Input placeholder={"Senha"} className={"login-password"} placeholder={"Senha"} type={"password"} required/>
-          </FormGroup>
+          <Form margin={"80px auto"} width={"80%"}>
+            <FormGroup>
+              <Input placeholder={"nome@email.com"} className={"login-email"} placeholder={"E-mail"} type={"email"} required />
+            </FormGroup>
+            <FormGroup>
+              <Input placeholder={"Senha"} className={"login-password"} placeholder={"Senha"} type={"password"} required />
+            </FormGroup>
 
-          <CustomLinkRight to={"/reset"} margin={"0 auto 20px auto"}>
-            Esqueceu a senha?
+            <CustomLinkRight to={"/reset"} margin={"0 auto 20px auto"}>
+              Esqueceu a senha?
             </CustomLinkRight>
 
-          <ButtonSecondary secondary onClick={this.handleLogin}>
-            {logged ? "Logado" : "Fazer login"}
-          </ButtonSecondary>
-        </Form>
+            <ButtonSecondary secondary onClick={this.handleLogin}>
+              {logged ? "Logado" : "Fazer login"}
+            </ButtonSecondary>
+          </Form>
 
-        <H1 txCenter clWhite className={"js-status"} />
+          <H1 txCenter clWhite className={"js-status"} />
 
-        <FooterUser content="Não tem uma conta?" to="/registry" label="Inscrever-se" />
+          <FooterUser content="Não tem uma conta?" to="/registry" label="Inscrever-se" />
 
-      </PanelLeft>
+        </PanelLeft>
 
-      <PanelRight>
-        <Slide />
-      </PanelRight>
-    </div>
-  );
-}
+        <PanelRight>
+          <Slide />
+        </PanelRight>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
