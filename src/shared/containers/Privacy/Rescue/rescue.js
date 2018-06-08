bbf232dc5b393ed916827cd5b928ffe92f18bfb1
Guilherme Guercione <guilherme.vecino@lunes.io>
2018-06-08 12:00:05 -0300

    Tabs Implementation

---
+++ src/shared/containers/Privacy/Rescue/rescue.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import style from "Shared/style-variables";
+
+// COMPONENTS
+import { H1 } from "Components/H1";
+
+class Rescue extends React.Component {  
+	render() {
+		return (
+      <div>
+				<H1>TESTE</H1>
+			</div>
+		);
+	}
+}
+
+export default Rescue;
