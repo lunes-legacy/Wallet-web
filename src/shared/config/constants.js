df01ed057d853cc9ef8e778c12cf94a43b5bdb98
Leonardo Ribeiro <leo.ribeiro@lunes.io>
2018-06-18 14:28:04 -0300

    refactor of menu

--- src/shared/config/constants.js
+++ src/shared/config/constants.js
@@ -1,6 +1,6 @@
 let testnet           = 'TRUE'; //it should comes from .env, well... maybe
 const ENV             = 'development'; //|| 'production'
-const TESTNET         = testnet === 'TRUE' ? true : false;
+const TESTNET         = testnet === 'FALSE' ? true : false;
 const APICONFIG 	      = TESTNET === true ? 'LNSTESTNET' : 'LNS'; //Uso na geração de seed/endereços
 const LUNES_LIB_ENV   = 'staging'; //'staging' || 'development' || 'production'
 const LUNES_LIB_LOGIN = 'manual'//| 'auto' || 'manual', isso serve para fazer login automático para nao ficar apertando o botão de login sempre
