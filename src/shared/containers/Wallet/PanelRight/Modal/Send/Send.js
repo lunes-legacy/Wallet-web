b45000fdf5bf78ebfeac5fc6644b150e0a777fbf
Gabriel Sandes <gabriel.sandes@lunes.io>
2018-06-20 12:11:08 -0300

    changed placeholder of modal send

--- src/shared/containers/Wallet/PanelRight/Modal/Send/Send.js
+++ src/shared/containers/Wallet/PanelRight/Modal/Send/Send.js
@@ -394,7 +394,7 @@ class Send extends React.Component {
 									style={ this.state.invalidAmount ? { color: "red" } : { color: "white" } }
 									data-amount-type={'coin'}
 									className={'input-amount coin'}
-									placeholder={'0.00000000'} />
+									placeholder={'0'} />
 							</Row>
 						</Col>
 					</Row>
