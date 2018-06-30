111b7a9e289cd321f03e6408c3553566406389e0
Felipe Matheus Rosa <felipe@lunes.io>
2018-06-30 20:04:28 -0300

    fix blockexplorer link

--- src/shared/utils/crypto.js
+++ src/shared/utils/crypto.js
@@ -4,13 +4,17 @@ export const getTxidLink = (network, txid) => {
 	if (network.search(/lns/i) !== -1) {
 		network = 'LUNES';
 	}
-	network      = network.toUpperCase();
-	let testnet  = TESTNET ? 'TESTNET' : '';
-	network      = network + testnet;
-	let explorer = BLOCK_EXPLORERS[network];
+	let networkConstant      = network.toUpperCase();
+	let testnetConstant  = TESTNET ? 'TESTNET' : '';
+	networkConstant      = networkConstant + testnetConstant;
+	let explorer = BLOCK_EXPLORERS[networkConstant];
+
+	let networkBlockExplorer      = network.toUpperCase();
+	let testnetBlockExplorer  = TESTNET ? 'TEST' : '';
+	networkBlockExplorer      = networkBlockExplorer + testnetBlockExplorer;
 
 	if (network.indexOf('LUNES') === -1) {
-		return `${explorer}api/v2/tx/${network}/${txid}`
+		return `${explorer}tx/${networkBlockExplorer}/${txid}`
 	}
 	return `${explorer}tx/${txid}`;
 }
